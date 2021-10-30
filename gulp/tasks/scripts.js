00623ffab8e9a055914204bb2bc7f91be98742e9
TimurZ <timurztv@gmail.com>
2021-10-30 21:03:24 +0300

    feat: removes @babel/plugin-transform-runtime

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -21,16 +21,6 @@ const webpackConfig = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              '@babel/plugin-proposal-export-default-from',
-              '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-proposal-optional-chaining',
-              '@babel/plugin-proposal-nullish-coalescing-operator',
-              '@babel/plugin-transform-runtime',
-            ],
-          },
         },
       },
     ],
