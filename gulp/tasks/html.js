e32eb6aae31559175ce72c4382ddfb7bc774cf96
TimurZ <timurztv@gmail.com>
2021-12-08 20:58:47 +0200

    chore: remove gulp-htmlmin

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,18 +1,11 @@
 const gulp = require('gulp');
 const rigger = require('gulp-rigger');
-const htmlmin = require('gulp-htmlmin');
-const mode = require('gulp-mode')();
 const paths = require('../paths');
-
-const htmlminConfig = {
-  collapseWhitespace: true,
-};
 
 const html = () => {
   return gulp
     .src(paths.src.html)
     .pipe(rigger())
-    .pipe(mode.production(htmlmin(htmlminConfig)))
     .pipe(gulp.dest(paths.build.html));
 };
 
