7a45db267bbd93389605815b4f94c32d6af95a3d
TimurZ <timurztv@gmail.com>
2021-12-08 20:33:46 +0200

    chore: update gulp constants

--- gulp/utils/constants.js
+++ gulp/utils/constants.js
@@ -1,8 +1,4 @@
-const IS_DEV = process.env.MODE === 'development';
-
-const IS_PROD = process.env.MODE === 'production';
-
 module.exports = {
-  IS_DEV,
-  IS_PROD,
+  IS_DEV: process.env.MODE === 'development',
+  IS_PROD: process.env.MODE === 'production',
 };
